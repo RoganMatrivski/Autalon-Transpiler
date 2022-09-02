3ad433df5754811db6070a36bb82dd8c0ce21ad4
Robin Mauritz <robinmauritzrm@gmail.com>
2022-09-02 23:17:21 +0700

    Separate lib and main function

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,20 @@ extern crate pest_derive;
 #[macro_use]
 extern crate lazy_static;
 
+use std::fs;
+
 use pest::{iterators::Pair, Parser};
 
 #[derive(Parser)]
 #[grammar = "parser/expr.pest"]
 struct AutalonParser;
-
-// mod builtin_pkg_list;
 mod checker;
 mod common_func;
 mod transpiler;
-// mod config;
 
-mod utils;
-
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, autalon-transpiler-wasm!");
-}
-
-#[wasm_bindgen]
-pub fn transpile_groovy(code: &str) -> String {
-    let parsed = AutalonParser::parse(Rule::program, code).expect("Failed to parse");
+fn main() {
+    let unparsed_file = fs::read_to_string("testfile.atln").expect("cannot read file");
+    let parsed = AutalonParser::parse(Rule::program, &unparsed_file).expect("Failed to parse");
 
     let mut checked_pair: Vec<Pair<Rule>> = vec![];
 
@@ -57,5 +36,5 @@ pub fn transpile_groovy(code: &str) -> String {
         }
     }
 
-    transpiler::program_handler(checked_pair).unwrap()
+    println!("{}", transpiler::program_handler(checked_pair).unwrap());
 }
